7758ffc1304669e14f65a48131a321ca4c243632
Gabriel Drozdov <gabrieldrozdov@gmail.com>
2026-06-10 18:34:28 -0400

    launch

---
+++ data.js
@@ -0,0 +1,388 @@
+let businesses = {
+	"Shop": [
+		{
+			"name": "The Little Bubblegum Shop",
+			"address": "103 Wickenden St.",
+			"googleMapsLink": "https://www.google.com/maps/place/The+Little+Bubblegum+Bookshop/@41.8181545,-71.4018128,17z/data=!3m1!4b1!4m6!3m5!1s0x89e445015d4ec5e9:0xc89414f1d0ffcdbc!8m2!3d41.8181545!4d-71.4018128!16s%2Fg%2F11xz3_tdxk?entry=ttu&g_ep=EgoyMDI2MDYwMy4xIKXMDSoASAFQAw%3D%3D",
+			"phone": "401-400-1241",
+			"url": "https://www.thelittlebubblegumbookshop.com/",
+			"image": "assets/images/littlebubblegum.jpg",
+			"latitude": 41.8181545,
+			"longitude": -71.4018128
+		},
+		{
+			"name": "Botanic Providence",
+			"address": "105 Wickenden St.",
+			"googleMapsLink": "https://www.google.com/maps/place/Botanic+Providence/@41.8181432,-71.4017744,17z/data=!3m1!4b1!4m6!3m5!1s0x89e445fe9cb5a339:0xbd0d99de23d4d89f!8m2!3d41.8181432!4d-71.4017744!16s%2Fg%2F11v9v7q2fc?entry=ttu&g_ep=EgoyMDI2MDYwMy4xIKXMDSoASAFQAw%3D%3D",
+			"phone": "",
+			"url": "http://www.botanicpvd.com/",
+			"image": "",
+			"latitude": 41.8181432,
+			"longitude": -71.4017744
+		},
+		{
+			"name": "Seymour Foods",
+			"address": "109 Wickenden St.",
+			"googleMapsLink": "https://www.google.com/maps/place/Seymour+Foods/@41.8179826,-71.4019778,17z/data=!3m1!4b1!4m6!3m5!1s0x89e445b9222a46f5:0xb32696111e171bb9!8m2!3d41.8179826!4d-71.4019778!16s%2Fg%2F11qb5lthdb?entry=ttu&g_ep=EgoyMDI2MDYwMy4xIKXMDSoASAFQAw%3D%3D",
+			"phone": "401-400-0440",
+			"url": "https://seymourfoodsne.com/",
+			"image": "",
+			"latitude": 41.8179826,
+			"longitude": -71.4019778
+		},
+		{
+			"name": "Adler’s Hardware",
+			"address": "173 Wickenden St.",
+			"googleMapsLink": "https://www.google.com/maps/place/Adler's+Design+Center+%26+Hardware/@41.8191039,-71.3997696,17z/data=!3m1!4b1!4m6!3m5!1s0x89e44538cec7ac31:0xf0d008f7041938f7!8m2!3d41.8191039!4d-71.3997696!16s%2Fg%2F1th4bdrr?entry=ttu&g_ep=EgoyMDI2MDYwMy4xIKXMDSoASAFQAw%3D%3D",
+			"phone": "401-421-5157",
+			"url": "https://www.adlersri.com/",
+			"image": "",
+			"latitude": 41.8191039,
+			"longitude": -71.3997696
+		},
+		{
+			"name": "Studio Flowers",
+			"address": "174 Wickenden St.",
+			"googleMapsLink": "https://www.google.com/maps/place/Studio+Flowers/@41.8193716,-71.399857,17z/data=!3m1!4b1!4m6!3m5!1s0x89e44538cdf35c2b:0x47023c8da6508132!8m2!3d41.8193716!4d-71.399857!16s%2Fg%2F1td_65yz?entry=ttu&g_ep=EgoyMDI2MDYwMy4xIKXMDSoASAFQAw%3D%3D",
+			"phone": "401-454-4400",
+			"url": "https://studioflowersri.com/",
+			"image": "",
+			"latitude": 41.8193716,
+			"longitude": -71.399857
+		},
+		{
+			"name": "NAVA",
+			"address": "197 Wickenden St.",
+			"googleMapsLink": "https://www.google.com/maps/place/NAVA/@41.8192457,-71.3992176,17z/data=!3m1!4b1!4m6!3m5!1s0x89e4452378d67791:0xf4eaee085d4197b!8m2!3d41.8192457!4d-71.3992176!16s%2Fg%2F1trpm6nr?entry=ttu&g_ep=EgoyMDI2MDYwMy4xIKXMDSoASAFQAw%3D%3D",
+			"phone": "401-453-6282",
+			"url": "https://shopnava.com/",
+			"image": "assets/images/nava.jpg",
+			"latitude": 41.8192457,
+			"longitude": -71.3992176
+		},
+		{
+			"name": "Nostalgia Antiques & Collectibles",
+			"address": "236 Wickenden St.",
+			"googleMapsLink": "https://www.google.com/maps/place/Nostalgia+Antiques+And+Collectibles+Store/@41.8195966,-71.3985324,17z/data=!3m1!4b1!4m6!3m5!1s0x89e44538e108ac0b:0xe2abd329d3742432!8m2!3d41.8195966!4d-71.3985324!16s%2Fg%2F1ptv_2bpr?entry=ttu&g_ep=EgoyMDI2MDYwMy4xIKXMDSoASAFQAw%3D%3D",
+			"phone": "401-400-5810",
+			"url": "https://www.nostalgiaprovidence.com/",
+			"image": "assets/images/nostalgia.jpg",
+			"latitude": 41.8195966,
+			"longitude": -71.3985324
+		},
+		{
+			"name": "Bloom Collective",
+			"address": "243 Wickenden St.",
+			"googleMapsLink": "https://www.google.com/maps/place/Bloom+Collective/@41.8193495,-71.3983898,17z/data=!3m1!4b1!4m6!3m5!1s0x89e445e424c27f85:0x1499716159924366!8m2!3d41.8193495!4d-71.3983898!16s%2Fg%2F11mvn07kmv?entry=ttu&g_ep=EgoyMDI2MDYwMy4xIKXMDSoASAFQAw%3D%3D",
+			"phone": "",
+			"url": "https://www.wearebloomcollective.com/",
+			"image": "",
+			"latitude": 41.8193495,
+			"longitude": -71.3983898
+		},
+		{
+			"name": "Urban Thread",
+			"address": "117 Brook St.",
+			"googleMapsLink": "https://www.google.com/maps/place/Urban+Thread/@41.8197571,-71.3981086,17z/data=!3m1!4b1!4m6!3m5!1s0x89e445d3ee62e4bf:0xfcdccb6f349db1ed!8m2!3d41.8197571!4d-71.3981086!16s%2Fg%2F11nnv4wv4t?entry=ttu&g_ep=EgoyMDI2MDYwMy4xIKXMDSoASAFQAw%3D%3D",
+			"phone": "401-654-6935",
+			"url": "https://www.urbanthreadpvd.com/",
+			"image": "assets/images/urbanthread.jpg",
+			"latitude": 41.8197571,
+			"longitude": -71.3981086
+		},
+		{
+			"name": "Campus Fine Wines",
+			"address": "127 Brook St.",
+			"googleMapsLink": "https://www.google.com/maps/place/Campus+Fine+Wines/@41.820106,-71.398238,16z/data=!3m1!4b1!4m6!3m5!1s0x89e445391b1a1369:0x4b4fa2b620e5ccb7!8m2!3d41.820106!4d-71.398238!16s%2Fg%2F1tdb593p?entry=ttu&g_ep=EgoyMDI2MDYwMy4xIKXMDSoASAFQAw%3D%3D",
+			"phone": "401-621-9650",
+			"url": "https://www.campusfinewines.com/",
+			"image": "assets/images/campus.jpg",
+			"latitude": 41.820106,
+			"longitude": -71.398238
+		},
+		{
+			"name": "Pretty Snake",
+			"address": "142 Brook St.",
+			"googleMapsLink": "https://www.google.com/maps/place/Pretty+Snake/@41.8204547,-71.3985158,17z/data=!3m1!4b1!4m6!3m5!1s0x89e4450a749f81f1:0x96b89f7dcb6aa7df!8m2!3d41.8204547!4d-71.3985158!16s%2Fg%2F11bw50f89q?entry=ttu&g_ep=EgoyMDI2MDYwMy4xIKXMDSoASAFQAw%3D%3D",
+			"phone": "401-388-0660",
+			"url": "https://prettysnake.com/",
+			"image": "",
+			"latitude": 41.8204547,
+			"longitude": -71.3985158
+		},
+		{
+			"name": "Knit Club",
+			"address": "144 Brook St.",
+			"googleMapsLink": "https://www.google.com/maps/place/KNIT+CLUB/@41.820476,-71.398466,17z/data=!3m1!4b1!4m6!3m5!1s0x89e445aebd7da13d:0x4e33446d07a8125d!8m2!3d41.820476!4d-71.398466!16s%2Fg%2F11y540ygj0?entry=ttu&g_ep=EgoyMDI2MDYwMy4xIKXMDSoASAFQAw%3D%3D",
+			"phone": "401-460-5648",
+			"url": "https://knit.club/",
+			"image": "assets/images/knitclub.jpg",
+			"latitude": 41.820476,
+			"longitude": -71.398466
+		},
+		{
+			"name": "Dougie’s Office",
+			"address": "163 Brook St.",
+			"googleMapsLink": "https://www.google.com/maps/place/Dougie's+Office/@41.8210012,-71.3983016,17z/data=!3m1!4b1!4m6!3m5!1s0x89e44500450f5591:0x6e0cbdd8277ba458!8m2!3d41.8210012!4d-71.3983016!16s%2Fg%2F11npmr9cpq?entry=ttu&g_ep=EgoyMDI2MDYwMy4xIKXMDSoASAFQAw%3D%3D",
+			"phone": "",
+			"url": "https://dougiesoffice.com/",
+			"image": "assets/images/dougies.jpg",
+			"latitude": 41.8210012,
+			"longitude": -71.3983016
+		},
+		{
+			"name": "Mission Electric Bike Shop",
+			"address": "181 Brook St.",
+			"googleMapsLink": "https://www.google.com/maps/place/Mission+Electric+Bike/@41.8214742,-71.3982753,17z/data=!3m1!4b1!4m6!3m5!1s0x89e445305eacd461:0x7d9e08fa85d4e59f!8m2!3d41.8214742!4d-71.3982753!16s%2Fg%2F11gh0lq77v?entry=ttu&g_ep=EgoyMDI2MDYwMy4xIKXMDSoASAFQAw%3D%3D",
+			"phone": "401-423-4400",
+			"url": "http://missionelectricbike.com/",
+			"image": "assets/images/missionelectric.jpg",
+			"latitude": 41.8214742,
+			"longitude": -71.3982753
+		},
+		{
+			"name": "Mister Sister Erotica",
+			"address": "268 Wickenden St.",
+			"googleMapsLink": "https://www.google.com/maps/place/Mister+Sister+Erotica/@41.8196542,-71.3977911,17z/data=!3m1!4b1!4m6!3m5!1s0x89e44538fdc68dd3:0xd84668e3322feb8b!8m2!3d41.8196542!4d-71.3977911!16s%2Fg%2F1tfn3rtp?entry=ttu&g_ep=EgoyMDI2MDYwMy4xIKXMDSoASAFQAw%3D%3D",
+			"phone": "401-421-6969",
+			"url": "",
+			"image": "assets/images/mistersister.jpg",
+			"latitude": 41.8196542,
+			"longitude": -71.3977911
+		},
+		{
+			"name": "Four Buds Floral Studio",
+			"address": "284 Wickenden St.",
+			"googleMapsLink": "https://www.google.com/maps/place/Four+Buds+Floral+Studio/@41.8196322,-71.3974587,17z/data=!3m1!4b1!4m6!3m5!1s0x89e4454d2de8c883:0x80422688c14e4380!8m2!3d41.8196322!4d-71.3974587!16s%2Fg%2F11sw6g8fv1?entry=ttu&g_ep=EgoyMDI2MDYwMy4xIKXMDSoASAFQAw%3D%3D",
+			"phone": "401-351-1775",
+			"url": "https://www.fourbudsfloral.com/",
+			"image": "assets/images/fourbuds.jpg",
+			"latitude": 41.8196322,
+			"longitude": -71.3974587
+		},
+		{
+			"name": "Project Attic Boutique",
+			"address": "301 Wickenden St.",
+			"googleMapsLink": "https://www.google.com/maps/place/Project+Attic+Boutique/@41.8194421,-71.3971655,17z/data=!3m1!4b1!4m6!3m5!1s0x89e4450029bc6cd7:0xe341ac4f33149618!8m2!3d41.8194421!4d-71.3971655!16s%2Fg%2F11vymxlxfy?entry=ttu&g_ep=EgoyMDI2MDYwMy4xIKXMDSoASAFQAw%3D%3D",
+			"phone": "401-277-0667",
+			"url": "https://project-attic.com/",
+			"image": "assets/images/projectattic.jpg",
+			"latitude": 41.8194421,
+			"longitude": -71.3971655
+		},
+		{
+			"name": "The Diamond Bar",
+			"address": "387 Wickenden St.",
+			"googleMapsLink": "https://www.google.com/maps/place/The+Diamond+Bar/@41.819679,-71.3954726,17z/data=!3m1!4b1!4m6!3m5!1s0x89e4459766531b5f:0x651ef5a45d23e736!8m2!3d41.819679!4d-71.3954726!16s%2Fg%2F11qrrp4kc3?entry=ttu&g_ep=EgoyMDI2MDYwMy4xIKXMDSoASAFQAw%3D%3D",
+			"phone": "401-533-9939",
+			"url": "https://www.thediamondbarri.com/",
+			"image": "",
+			"latitude": 41.819679,
+			"longitude": -71.3954726
+		},
+		{
+			"name": "Stewart House",
+			"address": "97 Hope St.",
+			"googleMapsLink": "https://www.google.com/maps/place/Stewart+House/@41.821837,-71.3963501,16z/data=!3m1!4b1!4m6!3m5!1s0x89e445157e0f8337:0x80c684c2711c6b78!8m2!3d41.821837!4d-71.3963501!16s%2Fg%2F11shsjf3qn?entry=ttu&g_ep=EgoyMDI2MDYwMy4xIKXMDSoASAFQAw%3D%3D",
+			"phone": "401-206-5252",
+			"url": "https://stewarthousepvd.com/",
+			"image": "",
+			"latitude": 41.821837,
+			"longitude": -71.3963501
+		},
+		{
+			"name": "Gallery Belleau",
+			"address": "424 Wickenden St.",
+			"googleMapsLink": "https://www.google.com/maps/place/Gallery+Belleau/@41.8201222,-71.3947727,17z/data=!3m1!4b1!4m6!3m5!1s0x89e44539de9e779d:0xe9a20b5c691b1d03!8m2!3d41.8201222!4d-71.3947727!16s%2Fg%2F1tf76jkd?entry=ttu&g_ep=EgoyMDI2MDYwMy4xIKXMDSoASAFQAw%3D%3D",
+			"phone": "401-456-0011",
+			"url": "https://www.gallerybelleau.org/",
+			"image": "",
+			"latitude": 41.8201222,
+			"longitude": -71.3947727
+		},
+		{
+			"name": "Three Wheel Studio",
+			"address": "436 Wickenden St.",
+			"googleMapsLink": "https://www.google.com/maps/place/Three+Wheel+Studio/@41.8202199,-71.3944509,17z/data=!3m1!4b1!4m6!3m5!1s0x89e44539db422f99:0xe679fdaebe8d480c!8m2!3d41.8202199!4d-71.3944509!16s%2Fg%2F12637tqk_?entry=ttu&g_ep=EgoyMDI2MDYwMy4xIKXMDSoASAFQAw%3D%3D",
+			"phone": "401-451-2350",
+			"url": "https://www.threewheelstudio.com/",
+			"image": "",
+			"latitude": 41.8202199,
+			"longitude": -71.3944509
+		},
+		{
+			"name": "Rustigian Rugs",
+			"address": "1 Governor St.",
+			"googleMapsLink": "https://www.google.com/maps/place/Rustigian+Rugs/@41.8202967,-71.3934501,17z/data=!3m1!4b1!4m6!3m5!1s0x89e44537431a6773:0x2513f2b8d7e79ba9!8m2!3d41.8202967!4d-71.3934501!16s%2Fg%2F1tg_mn79?entry=ttu&g_ep=EgoyMDI2MDYwMy4xIKXMDSoASAFQAw%3D%3D",
+			"phone": "401-751-5100",
+			"url": "https://rustigianrugs.com/",
+			"image": "",
+			"latitude": 41.8202967,
+			"longitude": -71.3934501
+		},
+	],
+	"Dine": [
+		{
+			"name": "Fellini Pizzeria",
+			"address": "166 Wickenden St.",
+			"googleMapsLink": "https://www.google.com/maps/place/Fellini+Pizzeria/@41.8193537,-71.4000588,16z/data=!3m1!4b1!4m6!3m5!1s0x89e44538cd0a8285:0x6fe0ae372bbab66!8m2!3d41.8193537!4d-71.4000588!16s%2Fg%2F1tcx2k1f?entry=ttu&g_ep=EgoyMDI2MDYwMy4xIKXMDSoASAFQAw%3D%3D",
+			"phone": "401-751-6737",
+			"url": "https://providence.fellinipizzeria.com/",
+			"image": "",
+			"latitude": 41.8193537,
+			"longitude": -71.4000588
+		},
+		{
+			"name": "The Coffee Exchange",
+			"address": "207 Wickenden St.",
+			"googleMapsLink": "https://www.google.com/maps/place/Coffee+Exchange/@41.819287,-71.3990847,17z/data=!3m1!4b1!4m6!3m5!1s0x89e44538dcb1b205:0x7f18d53736181c74!8m2!3d41.819287!4d-71.3990847!16s%2Fg%2F1tkl4b88?entry=ttu&g_ep=EgoyMDI2MDYwMy4xIKXMDSoASAFQAw%3D%3D",
+			"phone": "401-273-1198",
+			"url": "https://www.thecoffeeexchange.com/",
+			"image": "",
+			"latitude": 41.819287,
+			"longitude": -71.3990847
+		},
+		{
+			"name": "Amy’s Place",
+			"address": "214 Wickenden St.",
+			"googleMapsLink": "https://www.google.com/maps/place/Amy's/@41.8195837,-71.3990281,17z/data=!3m1!4b1!4m6!3m5!1s0x89e44538de9ae491:0xdf71381a1bf4cc2f!8m2!3d41.8195837!4d-71.3990281!16s%2Fg%2F1yfjd41lw?entry=ttu&g_ep=EgoyMDI2MDYwMy4xIKXMDSoASAFQAw%3D%3D",
+			"phone": "401-274-9966",
+			"url": "https://amysplaceri.com/",
+			"image": "",
+			"latitude": 41.8195837,
+			"longitude": -71.3990281
+		},
+		{
+			"name": "Sakura",
+			"address": "231 Wickenden St.",
+			"googleMapsLink": "https://www.google.com/maps/place/Sakura+Restaurant/@41.8193209,-71.3986387,17z/data=!3m1!4b1!4m6!3m5!1s0x89e44538e73deaa9:0xbad7074ed4e34e43!8m2!3d41.8193209!4d-71.3986387!16s%2Fg%2F1tgffcww?entry=ttu&g_ep=EgoyMDI2MDYwMy4xIKXMDSoASAFQAw%3D%3D",
+			"phone": "401-331-6861",
+			"url": "http://www.providencesakura.com/",
+			"image": "",
+			"latitude": 41.8193209,
+			"longitude": -71.3986387
+		},
+		{
+			"name": "Brickway on Wickenden",
+			"address": "234 Wickenden St.",
+			"googleMapsLink": "https://www.google.com/maps/place/Brickway+On+Wickenden/@41.819578,-71.398599,17z/data=!3m1!4b1!4m6!3m5!1s0x89e44538e12a5b97:0xc9c887637e61a114!8m2!3d41.819578!4d-71.398599!16s%2Fg%2F1tcxw7pr?entry=ttu&g_ep=EgoyMDI2MDYwMy4xIKXMDSoASAFQAw%3D%3D",
+			"phone": "401-751-2477",
+			"url": "https://brickwayonwickenden.com/home-page",
+			"image": "",
+			"latitude": 41.819578,
+			"longitude": -71.398599
+		},
+		{
+			"name": "Caspian Breakfast & Lunch",
+			"address": "234 Wickenden St.",
+			"googleMapsLink": "https://www.google.com/maps/place/caspian+breakfast+and+lunch/@41.8193495,-71.3983898,17z/data=!3m1!4b1!4m6!3m5!1s0x89e44576a10833eb:0xb4506eb096a7c049!8m2!3d41.8193495!4d-71.3983898!16s%2Fg%2F11ftml4vys?entry=ttu&g_ep=EgoyMDI2MDYwMy4xIKXMDSoASAFQAw%3D%3D",
+			"phone": "401-751-2477",
+			"url": "https://caspian-wickenden.com/",
+			"image": "assets/images/caspian.jpg",
+			"latitude": 41.8193495,
+			"longitude": -71.3983898
+		},
+		{
+			"name": "The Point Tavern",
+			"address": "302 Wickenden St.",
+			"googleMapsLink": "https://www.google.com/maps/place/The+Point+Tavern/@41.8196906,-71.3971397,17z/data=!3m1!4b1!4m6!3m5!1s0x89e44539aae00c6f:0xd9911d39530b7a5e!8m2!3d41.8196906!4d-71.3971397!16s%2Fg%2F1yg570vl2?entry=ttu&g_ep=EgoyMDI2MDYwMy4xIKXMDSoASAFQAw%3D%3D",
+			"phone": "401-751-4900",
+			"url": "https://www.thepointtavernpvd.com/",
+			"image": "assets/images/thepoint.jpg",
+			"latitude": 41.8196906,
+			"longitude": -71.3971397
+		},
+		{
+			"name": "Jahunger",
+			"address": "333 Wickenden St.",
+			"googleMapsLink": "https://www.google.com/maps/place/Jahunger+Providence/@41.8194556,-71.3964929,17z/data=!3m1!4b1!4m6!3m5!1s0x89e44539b2ba6967:0xa63ee6fea60fb8d9!8m2!3d41.8194556!4d-71.3964929!16s%2Fg%2F11g6yd33nm?entry=ttu&g_ep=EgoyMDI2MDYwMy4xIKXMDSoASAFQAw%3D%3D",
+			"phone": "401-454-6866",
+			"url": "https://www.jahunger.com/",
+			"image": "assets/images/jahunger.jpg",
+			"latitude": 41.8194556,
+			"longitude": -71.3964929
+		},
+		{
+			"name": "Small Format",
+			"address": "335 Wickenden St.",
+			"googleMapsLink": "https://www.google.com/maps/place/Small+Format/@41.819451,-71.3963235,17z/data=!3m1!4b1!4m6!3m5!1s0x89e4457051e2df3f:0x53c1df4142653d45!8m2!3d41.819451!4d-71.3963235!16s%2Fg%2F11lgf27bcz?entry=ttu&g_ep=EgoyMDI2MDYwMy4xIKXMDSoASAFQAw%3D%3D",
+			"phone": "",
+			"url": "https://www.smallformat.one/",
+			"image": "assets/images/smallformat.jpg",
+			"latitude": 41.819451,
+			"longitude": -71.3963235
+		},
+		{
+			"name": "Pizza Pie-er",
+			"address": "374 Wickenden St.",
+			"googleMapsLink": "https://www.google.com/maps/place/Pizza+Pie-er/@41.8198031,-71.3958403,17z/data=!3m1!4b1!4m6!3m5!1s0x89e44539b7cd664b:0x2eec3f1368326c50!8m2!3d41.8198031!4d-71.3958403!16s%2Fg%2F1tdxx0ph?entry=ttu&g_ep=EgoyMDI2MDYwMy4xIKXMDSoASAFQAw%3D%3D",
+			"phone": "401-351-3663",
+			"url": "https://pizza-pie-er-provindence-online-ordering.securebrygid.com/zgrid/proc/site/sitep.jsp",
+			"image": "",
+			"latitude": 41.8198031,
+			"longitude": -71.3958403
+		},
+		{
+			"name": "Rose Mart",
+			"address": "460 Wickenden St.",
+			"googleMapsLink": "https://www.google.com/maps/place/Rose+Mart/@41.8203355,-71.3940537,17z/data=!3m1!4b1!4m6!3m5!1s0x89e44557c06fd087:0xe3fae878c23b04b!8m2!3d41.8203355!4d-71.3940537!16s%2Fg%2F11xrvmcvpl?entry=ttu&g_ep=EgoyMDI2MDYwMy4xIKXMDSoASAFQAw%3D%3D",
+			"phone": "401-537-7218",
+			"url": "https://www.instagram.com/rosemartpvd",
+			"image": "assets/images/rosemart.jpg",
+			"latitude": 41.8203355,
+			"longitude": -71.3940537
+		},
+		{
+			"name": "Pizza Marvin",
+			"address": "468 Wickenden St.",
+			"googleMapsLink": "https://www.google.com/maps/place/Pizza+Marvin/@41.8204346,-71.3938124,17z/data=!3m1!4b1!4m6!3m5!1s0x89e445e5009ca6a3:0x1f832dbc7c2581e3!8m2!3d41.8204346!4d-71.3938124!16s%2Fg%2F11qp1cvwdt?entry=ttu&g_ep=EgoyMDI2MDYwMy4xIKXMDSoASAFQAw%3D%3D",
+			"phone": "401-262-3336",
+			"url": "https://www.pizzamarvin.com/",
+			"image": "",
+			"latitude": 41.8204346,
+			"longitude": -71.3938124
+		},
+		{
+			"name": "Club Frills",
+			"address": "79 Ives St.",
+			"googleMapsLink": "https://www.google.com/maps/place/Club+Frills/@41.8204238,-71.3916241,17z/data=!3m1!4b1!4m6!3m5!1s0x89e4451d78a05bb1:0x3e8c4a4686cee2cb!8m2!3d41.8204238!4d-71.3916241!16s%2Fg%2F11ychgyw80?entry=ttu&g_ep=EgoyMDI2MDYwMy4xIKXMDSoASAFQAw%3D%3D",
+			"phone": "",
+			"url": "https://www.clubfrillspvd.com/",
+			"image": "",
+			"latitude": 41.8204238,
+			"longitude": -71.3916241
+		}
+	],
+	"Services": [
+		{
+			"name": "Hair Lab",
+			"address": "328 Wickenden St.",
+			"googleMapsLink": "https://www.google.com/maps/place/Hairlab/@41.8196836,-71.3966641,17z/data=!3m1!4b1!4m6!3m5!1s0x89e44539ade788a7:0x3e578f2fa621be0b!8m2!3d41.8196836!4d-71.3966641!16s%2Fg%2F11b6gnqldm?entry=ttu&g_ep=EgoyMDI2MDYwMy4xIKXMDSoASAFQAw%3D%3D",
+			"phone": "401-421-7700",
+			"url": "https://www.hair-lab-salon.com/",
+			"image": "",
+			"latitude": 41.8196836,
+			"longitude": -71.3966641
+		},
+		{
+			"name": "Metro Nails",
+			"address": "395 Wickenden St.",
+			"googleMapsLink": "https://www.google.com/maps/place/Metro+Nails+%7C+Nail+Salon/@41.8197432,-71.3952074,17z/data=!3m1!4b1!4m6!3m5!1s0x89e44539cf56a799:0xb3fd36a5468d0b3e!8m2!3d41.8197432!4d-71.3952074!16s%2Fg%2F1td6t7l4?entry=ttu&g_ep=EgoyMDI2MDYwMy4xIKXMDSoASAFQAw%3D%3D",
+			"phone": "401-490-0101",
+			"url": "https://sites.google.com/view/metronails/home",
+			"image": "",
+			"latitude": 41.8197432,
+			"longitude":-71.3952074
+		},
+		{
+			"name": "Revive Salon",
+			"address": "442 Wickenden St.",
+			"googleMapsLink": "https://www.google.com/maps/place/Revive+Salon+(formerly+Ky+Michaels+Salon)/@41.820251,-71.394291,17z/data=!3m1!4b1!4m6!3m5!1s0x89e44537603ccf4f:0x1e7f808a641758c4!8m2!3d41.820251!4d-71.394291!16s%2Fg%2F1hc7zqgz3?entry=ttu&g_ep=EgoyMDI2MDYwMy4xIKXMDSoASAFQAw%3D%3D",
+			"phone": "401-228-3808",
+			"url": "https://www.revivesalonri.com/",
+			"image": "",
+			"latitude": 41.820251,
+			"longitude": -71.394291
+		}
+	]
+}
---
+++ script.js
@@ -0,0 +1,237 @@
+// init map
+let map;
+function initEverything() {
+	if (map) {
+		map.remove();
+		map = null;
+	}
+	map = L.map('wama-guide-map', {closePopupOnClick: false}).setView([41.8194262,-71.3966686], 16.25);
+	L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
+		attribution: '© OpenStreetMap contributors © CARTO',
+		minZoom: 16,
+		maxZoom: 19,
+	}).addTo(map);
+	map.setMaxBounds([[41.787997, -71.422022], [41.863128, -71.354635]]);
+	generateWAMAMap();
+}
+initEverything();
+
+let targetDevice = 'desktop';
+if (window.innerWidth < 800) {
+	targetDevice = 'mobile';
+}
+window.addEventListener('resize', () => {
+	console.log(1)
+	if (window.innerWidth < 800 && targetDevice == 'desktop') {
+		targetDevice = 'mobile';
+		setTimeout(initEverything, 50);
+	} else if (window.innerWidth >= 800 && targetDevice == 'mobile') {
+		targetDevice = 'desktop';
+		setTimeout(initEverything, 50);
+	}
+})
+
+// convert data to plain array
+let businessesRaw = [];
+for (let key in businesses) {
+	let section = businesses[key];
+	for (let biz of section) {
+		businessesRaw.push(biz);
+	}
+}
+
+function generateWAMAMap() {
+	let temp = '';
+	let jumpTemp = '';
+	let i = 1;
+	for (let key in businesses) {
+
+		let section = businesses[key];
+		let sectionTemp = '';
+
+		let primary = "purple";
+		if (key == "Shop") {
+			primary = "purple";
+		} else if (key == "Dine") {
+			primary = "orange";
+		} else if (key == "Services") {
+			primary = "pink";
+		}
+
+		jumpTemp += `
+			<a href="#${key.toLowerCase()}" class="wama-guide-jump-link" style="--primary: var(--wama-${primary});">
+				${key}
+			</a>
+		`;
+
+		for (let biz of section) {
+
+			// build address
+			let address = "";
+			if (biz.address != "") {
+				address = `<a href="${biz.googleMapsLink}" target="_blank" class="wama-guide-list-group-item-link">${biz.address}</a>`;
+			}
+
+			// build phone
+			let phone = "";
+			if (biz.phone != "") {
+				phone = `<a href="tel:${biz.phone}" class="wama-guide-list-group-item-link">${biz.phone}</a>`;
+			}
+
+			// build url
+			let title = biz.name;
+			if (biz.url != "") {
+				title = `<a href="${biz.url}" target="_blank">${biz.name}</a>`;
+			}
+
+			sectionTemp += `
+				<div onmouseenter="highlightOnMap(${i});" class="wama-guide-list-group-item" data-guide-index="${i}">
+					<div class="wama-guide-list-group-item-index">
+						${i}
+					</div>
+					<div class="wama-guide-list-group-item-content">
+						<h3 class="wama-guide-list-group-item-title">${title}</h3>
+						<div class="wama-guide-list-group-item-info">
+							${address}
+							${phone}
+						</div>
+					</div>
+				</div>
+			`;
+
+			L.marker([biz.latitude, biz.longitude], {
+				icon: L.divIcon({
+					className: `marker-${i}`,
+					html: `
+						<div class="wama-guide-map-marker" data-marker-index="${i}" style="--primary: var(--wama-${primary})" onmouseenter="highlightInList(${i});" onclick="showMapInfo(${i});">
+							<div class="wama-guide-map-marker-index">${i}</div>
+							<svg viewBox="0 0 76.63 95.52" class="wama-guide-map-marker-pin" preserveAspectRatio="none"><path d="M76.63,38.32c0,32.68-38.32,57.21-38.32,57.21,0,0-38.32-24.63-38.32-57.21C0,17.15,17.15,0,38.32,0s38.32,17.15,38.32,38.32Z"/></svg>
+						</div>`,
+					iconSize: [24, 30],
+					iconAnchor: [12, 30]
+				})
+			}).addTo(map);
+
+			i++;
+		}
+
+		temp += `
+			<div class="wama-guide-list-group" style="--primary: var(--wama-${primary});" id="${key.toLowerCase()}">
+				<h2 class="wama-guide-list-group-heading"><div><span>${key}</span><span>${key}</span><span>${key}</span><span>${key}</span><span>${key}</span><span>${key}</span><span>${key}</span><span>${key}</span><span>${key}</span><span>${key}</span><span>${key}</span><span>${key}</span><span>${key}</span><span>${key}</span><span>${key}</span><span>${key}</span><span>${key}</span><span>${key}</span><span>${key}</span><span>${key}</span><span>${key}</span><span>${key}</span><span>${key}</span><span>${key}</span><span>${key}</span><span>${key}</span><span>${key}</span><span>${key}</span><span>${key}</span><span>${key}</span><span>${key}</span><span>${key}</span><span>${key}</span><span>${key}</span><span>${key}</span><span>${key}</span><span>${key}</span><span>${key}</span><span>${key}</span><span>${key}</span></div><div><span>${key}</span><span>${key}</span><span>${key}</span><span>${key}</span><span>${key}</span><span>${key}</span><span>${key}</span><span>${key}</span><span>${key}</span><span>${key}</span><span>${key}</span><span>${key}</span><span>${key}</span><span>${key}</span><span>${key}</span><span>${key}</span><span>${key}</span><span>${key}</span><span>${key}</span><span>${key}</span><span>${key}</span><span>${key}</span><span>${key}</span><span>${key}</span><span>${key}</span><span>${key}</span><span>${key}</span><span>${key}</span><span>${key}</span><span>${key}</span><span>${key}</span><span>${key}</span><span>${key}</span><span>${key}</span><span>${key}</span><span>${key}</span><span>${key}</span><span>${key}</span><span>${key}</span><span>${key}</span></div></h2>
+				<div class="wama-guide-list-group-items">
+					${sectionTemp}
+				</div>
+			</div>
+		`;
+	}
+	let elmnt = document.querySelector('.wama-guide-list');
+	elmnt.innerHTML = `
+		${temp}
+		<div class="wama-guide-jump">
+			${jumpTemp}
+		</div>
+	`;
+}
+
+// highlight from list interaction
+function highlightOnMap(index) {
+	if (window.innerWidth < 800) {
+		return
+	}
+
+	// unhighlight everything
+	for (let item of document.querySelectorAll(`.wama-guide-list-group-item`)) {
+		item.dataset.active = 0
+	}
+	for (let item of document.querySelectorAll(`.leaflet-marker-icon`)) {
+		item.dataset.active = 0
+	}
+
+	// highlight item
+	let guideItem = document.querySelector(`.wama-guide-list-group-item[data-guide-index="${index}"]`);
+	guideItem.dataset.active = 1;
+	let markerItem = document.querySelector(`.marker-${index}`);
+	markerItem.dataset.active = 1;
+
+	// fly to location
+	let currentBiz = businessesRaw[index-1];
+	let offset = 0;
+	if (currentBiz.image != "") {
+		offset = 0.0005;
+	}
+	map.flyTo([currentBiz.latitude + offset, currentBiz.longitude], 18);
+
+	showMapInfo(index)
+}
+
+// highlight from map interaction
+function highlightInList(index) {
+	if (window.innerWidth < 800) {
+		return
+	}
+
+	// unhighlight everything
+	for (let item of document.querySelectorAll(`.wama-guide-list-group-item`)) {
+		item.dataset.active = 0
+	}
+	for (let item of document.querySelectorAll(`.leaflet-marker-icon`)) {
+		item.dataset.active = 0
+	}
+
+	// highlight item
+	let guideItem = document.querySelector(`.wama-guide-list-group-item[data-guide-index="${index}"]`);
+	guideItem.dataset.active = 1;
+	let markerItem = document.querySelector(`.marker-${index}`);
+	markerItem.dataset.active = 1;
+
+	// scroll to list item
+	guideItem.scrollIntoView({
+		behavior: "smooth",
+		block: "center"
+	});
+}
+
+// show popup on map
+function showMapInfo(index) {
+	let biz = businessesRaw[index-1];
+
+	// build address
+	let address = "";
+	if (biz.address != "") {
+		address = `<a href="${biz.googleMapsLink}" target="_blank" class="wama-guide-map-popup-link">${biz.address}</a>`;
+	}
+
+	// build phone
+	let phone = "";
+	if (biz.phone != "") {
+		phone = `<a href="tel:${biz.phone}" class="wama-guide-map-popup-link">${biz.phone}</a>`;
+	}
+
+	// build url
+	let title = biz.name;
+	if (biz.url != "") {
+		title = `<a href="${biz.url}" target="_blank">${biz.name}</a>`;
+	}
+
+	// build image
+	let image = "";
+	if (biz.image != "") {
+		image = `<img src="${biz.image}" class="wama-guide-map-popup-image">`;
+	}
+
+	popupTemp = `
+		<div class="wama-guide-map-popup">
+			${image}
+			<h3 class="wama-guide-map-popup-title">${title}</h3>
+			<div class="wama-guide-map-popup-info">
+				${address}
+				${phone}
+			</div>
+		</div>
+	`;
+
+	L.popup()
+		.setLatLng([biz.latitude, biz.longitude])
+		.setContent(popupTemp)
+		.openOn(map);
+}
